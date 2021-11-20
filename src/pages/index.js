2f3726d5ee530370f0ca352131ad732a608c3fdb
João Belo <joaobelo.dev@gmail.com>
2021-11-20 17:33:43 +0000

    cleanup init. add dependencies. config prettier. add jsonserver db and delay middleware

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Fuze Calls Challenge</title>
+      </Head>
+
+      <main>
+        <h1>main</h1>
+      </main>
+
+      <footer>
+        <h1>footer</h1>
+      </footer>
+    </div>
+  )
+}
